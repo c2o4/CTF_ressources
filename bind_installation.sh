cac541146b55e630e48ab93d7b3ad8cc3d82b90f
c2o4 <c2o4@wanadoo.fr>
2020-12-07 16:05:40 +0100

    Add files via upload

--- bind_installation.sh
+++ bind_installation.sh
@@ -3,11 +3,14 @@ cd /etc
 sudo git clone https://github.com/c2o4/CTF_ressources.git
 sudo mv CTF_ressources/ bind/
 sudo mv bind/named.conf ./named.conf
+sudo mv bind/bind9.service /etc/systemd/system/bind9.service
 cd bind/
 sudo tar xzvf bind-9.4.2.tar.gz
 cd bind-9.4.2
 ./configure
 make
 sudo make install
-# start named and bind9 at boot ???
+sudo named -g #Checks configuration
+sudo crontab -e #add @reboot named
+sudo nano /etc/netplan/00-installer-config.yaml
 sudo reboot
